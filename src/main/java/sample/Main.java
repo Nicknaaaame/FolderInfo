609edb02af53ad349c0e29e0e9254f64fb96521f
Nicknaaaame <48119336+Nicknaaaame@users.noreply.github.com>
2019-12-19 16:58:01 +0700

    test commit

--- src/main/java/sample/Main.java
+++ src/main/java/sample/Main.java
@@ -4,26 +4,37 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.TextArea;
 import javafx.stage.Stage;
+import sample.view.Activity;
 
 import java.net.URI;
 import java.net.URL;
 import java.nio.file.Paths;
+import java.util.HashMap;
+import java.util.Map;
 import java.util.Objects;
 
 public class Main extends Application {
     private static Stage staticPrimaryStage;
-    private static Parent staticRoot;
+    private static FXMLLoader loader;
 
     public static Stage getPrimaryStage(){
         return staticPrimaryStage;
+    }
+
+    public static FXMLLoader getLoader(){
+        return loader;
     }
 
     @Override
     public void start(Stage primaryStage) throws Exception{
         staticPrimaryStage = primaryStage;
         URL url = Paths.get("./src/main/java/sample/view/view.fxml").toUri().toURL();
-        Parent root = FXMLLoader.load(url);
+        loader = new FXMLLoader(url);
+        Parent root = loader.load();
+        //lox
+        new Activity();
         primaryStage.setTitle("Hello World");
         primaryStage.setScene(new Scene(root, 1200, 700));
         primaryStage.show();
--- src/main/java/sample/view/Activity.java
+++ src/main/java/sample/view/Activity.java
@@ -1,32 +1,22 @@
 package sample.view;
 
-import javafx.fxml.FXML;
 import javafx.scene.control.MenuItem;
 import javafx.scene.control.TextArea;
-import javafx.scene.layout.StackPane;
 import javafx.stage.Stage;
 import sample.Main;
 import sample.presenter.Presenter;
 
-import java.io.File;
-
 public class Activity {
-    @FXML
     private TextArea taMain;
-    @FXML
-    private MenuItem miOpen;
-    @FXML
-    private StackPane spMain;
 
     private Stage primaryStage;
 
     private Presenter presenter;
 
     public Activity() {
-    }
-
-    public void initialize(){
-        presenter = new Presenter(taMain, spMain);
+        presenter = new Presenter();
+        taMain = (TextArea) Main.getLoader().getNamespace().get("taMain");
+        MenuItem miOpen = (MenuItem) Main.getLoader().getNamespace().get("miOpen");
 
         primaryStage = Main.getPrimaryStage();
         primaryStage.heightProperty().addListener(e->{
@@ -36,9 +26,5 @@ public class Activity {
             taMain.setPrefWidth(primaryStage.getWidth());
         });
         miOpen.setOnAction(e-> presenter.onDirChoose(e));
-    }
-
-    public StackPane getSpMain() {
-        return spMain;
     }
 }
